5c3bda3d8b414c8ffe6ae1dfc67c65cc7d94843d
z4yx <z4yx@users.noreply.github.com>
2022-10-23 23:33:56 +0800

    initial version

---
+++ app/src/main/java/com/example/wifienabler/AlarmBroadcastReceiver.kt
@@ -0,0 +1,123 @@
+package com.example.wifienabler
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.*
+import android.net.wifi.WifiManager
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AlarmBroadcastReceiver : BroadcastReceiver() {
+    companion object {
+        const val CHANNEL_ID = "id1233"
+
+        fun getPerf(context: Context): SharedPreferences? {
+            return context.getSharedPreferences(context.getString(R.string.event_log_perf), Context.MODE_PRIVATE)
+        }
+        fun appendLog(logs : String, newOne : String) : String {
+            var items = logs.split("\n")
+            if (items.size > 9) {
+                items = items.subList(items.size-9, items.size)
+            }
+            return items.joinToString("\n") + "\n" + newOne
+        }
+        fun logEvent(context: Context?, content: String) {
+            if (context != null) {
+                val perf = getPerf(context)
+                val log = perf?.getString("eventLog", "")!!
+                with(perf.edit()) {
+                    this?.putString("eventLog", appendLog(log, content))
+                    this?.apply()
+                }
+            }
+        }
+        fun getEvents(context: Context?) : String{
+            return getPerf(context!!)?.getString("eventLog", "").toString()
+        }
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val curDateTime = SimpleDateFormat("MM.dd HH:mm:ss", Locale.getDefault()).format(Date())
+        var text = "Turn on WiFi at " + curDateTime
+
+        Log.i("WiFiEnablerBroadcast", "Alarm fires")
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+
+        val succ = enableWiFi(context)
+        text += " " + succ.toString()
+        logEvent(context, text)
+        if(!succ && context!=null) {
+            val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("WiFi")
+                .setContentText("Please enable WiFi manually")
+                .setContentIntent(PendingIntent.getActivity(context, 0, Intent(), 0))
+                .setAutoCancel(true)
+            with(NotificationManagerCompat.from(context)) {
+                // notificationId is a unique int for each notification that you must define
+                val notificationId = 103451
+                notify(notificationId, builder.build())
+            }
+
+//            launchWiFiSetting(context)
+        }
+
+        if (context != null)
+            registerAlarm(context)
+    }
+
+    fun launchWiFiSetting(context: Context?) {
+//        val cn = ComponentName("com.android.settings","com.android.settings.wifi.WifiSettings")
+        with(Intent(Settings.ACTION_WIFI_SETTINGS)) {
+//            setComponent(cn)
+            setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            try {
+                context?.startActivity(this)
+            } catch (exp: Exception) {
+                Log.e("WiFiEnablerBroadcast", exp.message.toString())
+            }
+        }
+    }
+
+    fun enableWiFi(context: Context?) : Boolean {
+        val wifiMan = context?.getApplicationContext()?.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        if (wifiMan.isWifiEnabled) {
+            return true
+        }
+        return wifiMan.setWifiEnabled(true)
+    }
+
+    fun registerAlarm(ctx: Context) {
+        val appCtx = ctx.applicationContext
+        val alarmMan = appCtx.getSystemService(Context.ALARM_SERVICE) as AlarmManager?
+        val intent = Intent(appCtx, AlarmBroadcastReceiver::class.java)
+        val pendIntent = PendingIntent.getBroadcast(appCtx, 123, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+        val calendar = Calendar.getInstance().apply {
+            timeInMillis = System.currentTimeMillis()
+            if (get(Calendar.HOUR_OF_DAY) >= 17) {
+                add(Calendar.DAY_OF_MONTH, 1)
+            }
+            set(Calendar.HOUR_OF_DAY, 17)
+            set(Calendar.MINUTE, 25)
+        }
+        alarmMan?.setWindow(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, 10*60*1000, pendIntent)
+
+        val pendIntent2 = PendingIntent.getBroadcast(appCtx, 124, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+        with(calendar) {
+            timeInMillis = System.currentTimeMillis()
+            if (get(Calendar.HOUR_OF_DAY) >= 8) {
+                add(Calendar.DAY_OF_MONTH, 1)
+            }
+            set(Calendar.HOUR_OF_DAY, 8)
+            set(Calendar.MINUTE, 50)
+        }
+        alarmMan?.setWindow(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, 10*60*1000, pendIntent2)
+
+    }
+}
---
+++ app/src/main/java/com/example/wifienabler/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.wifienabler
+
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.net.wifi.WifiManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.getSystemService
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        createNotificationChannel()
+
+//        registerAlarm()
+//        enableWiFi()
+        refershEventView(findViewById<TextView>(R.id.eventsView))
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(AlarmBroadcastReceiver.CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+
+    fun onClickTrunOnWiFi(view: View) {
+        val recv = AlarmBroadcastReceiver()
+        recv.onReceive(this, null)
+    }
+
+    fun registerAlarm(view: View) {
+        val recv = AlarmBroadcastReceiver()
+        recv.registerAlarm(this)
+
+        Toast.makeText(this, "Alarm Registered", Toast.LENGTH_SHORT).show()
+    }
+
+    fun refershEventView(view: View) {
+        val events = AlarmBroadcastReceiver.getEvents(this)
+        (view as TextView).setText(events)
+    }
+}
